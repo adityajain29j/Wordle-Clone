d995a4fed0e0e4a5af1d7b826f7f6e40e0eef625
Aditya Jain <aditya_jain@mckinsey.com>
2023-12-04 01:12:43 +0530

    Add functionality to keybaord

--- src/components/Keyboard/Keyboard.jsx
+++ src/components/Keyboard/Keyboard.jsx
@@ -2,37 +2,40 @@ import React from "react";
 import KeyboardKey from "../KeyboardKey/KeyboardKey";
 import "./Keyboard.css";
 
-const Keyboard = ({ keyColors }) => {
+const Keyboard = ({ keyColors, clickLetter }) => {
   const keyboardTopRow = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keyboardMiddleRow = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
-  const keyboardBottomRow = ["Z", "X", "C", "V", "B", "N", "M"];
+  const keyboardBottomRow = ["↵", "Z", "X", "C", "V", "B", "N", "M","←"];
 
   return (
     <div className="keyboard">
-      <div className="keyboard-toprow">
+      <div className="keyboard-key-row">
         {keyboardTopRow.map((keyLetter) => (
           <KeyboardKey
             key={keyLetter}
             letter={keyLetter}
             keyColor={keyColors ? keyColors[keyLetter] : ""}
+            letterOnClick={clickLetter}
           />
         ))}
       </div>
-      <div className="keyboard-middlerow">
+      <div className="keyboard-key-row">
         {keyboardMiddleRow.map((keyLetter) => (
           <KeyboardKey
             key={keyLetter}
             letter={keyLetter}
             keyColor={keyColors ? keyColors[keyLetter] : ""}
+            letterOnClick={clickLetter}
           />
         ))}
       </div>
-      <div className="keyboard-bottomrow">
+      <div className="keyboard-key-row">
         {keyboardBottomRow.map((keyLetter) => (
           <KeyboardKey
             key={keyLetter}
             letter={keyLetter}
             keyColor={keyColors ? keyColors[keyLetter] : ""}
+            letterOnClick={clickLetter}
           />
         ))}
       </div>
